289e7fc0093f4346d785596d09935a6feb0fd5a1
Raj Das <mail.rajdas@gmail.com>
2018-03-06 00:03:54 +0530

    commited

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # mozillaclub
-<<<<<<< HEAD
 **Link** - https://mozilla-bbsr.ml
 ### Installing
 ```
@@ -15,6 +14,4 @@ ExpressJS
 ```
 ### Authors
 * Created and Maintain By [Raj Das](https:fb.com/itsrajdas)
-=======
 Mozilla Club WebSite
->>>>>>> ac57006e9f822a066802c8b12ab191cdf8746e3b
--- schema.js
+++ schema.js
@@ -1,10 +1,7 @@
 var mongoose=require('mongoose');
 var schema = mongoose.Schema;
 var myschema=new schema({
-<<<<<<< HEAD
   _id:{unique: true,type: String},
-=======
->>>>>>> ac57006e9f822a066802c8b12ab191cdf8746e3b
   name:{unique: false,type: String},
   gender:{unique: false,type: String},
   email:{unique: false,type: String},
